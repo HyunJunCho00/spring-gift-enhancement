811043968340c957be56ff08d13faa15b59bf6cd
HyunJunCho00 <162411774+HyunJunCho00@users.noreply.github.com>
2025-07-14 07:48:36 -0700

    경북대 BE_조현준 STEP0 코드 복사 (#45)
    * feat: 코드 복사
    
    * Update README.md

---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,21 @@
+Create Table products(
+ id BIGINT AUTO_INCREMENT PRIMARY KEY,
+ name VARCHAR(255) NOT NULL,
+ price INT NOT NULL,
+ image_url VARCHAR(1000) NOT NULL
+);
+
+CREATE TABLE member (
+id BIGINT AUTO_INCREMENT PRIMARY KEY,
+email VARCHAR(255) NOT NULL UNIQUE,
+password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE wish(
+     id BIGINT AUTO_INCREMENT PRIMARY KEY,
+     member_id BIGINT NOT NULL,
+     product_id BIGINT NOT NULL,
+     FOREIGN KEY (member_id) REFERENCES member(id) ON DELETE CASCADE,
+     FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE,
+     UNIQUE (member_id, product_id)
+);
